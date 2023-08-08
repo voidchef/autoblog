f866de59e272c1e50288f09b1b868ba3fbae40b2
voidchef <neelutpal.saha@s.amity.edu>
2023-08-08 17:36:53 +0000

    chore: vite svg plugin

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/v1': {
-        target: 'http://localhost:5000/',
+        target: 'http://localhost:3000/',
         changeOrigin: true,
         secure: false,
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
