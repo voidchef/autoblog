7c952dec6660827e138fe4c1013a99f7c3adb121
Neelutpal Saha <79092620+voidchef@users.noreply.github.com>
2023-08-08 18:25:21 +0530

    Initial commit

---
+++ front/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/v1': {
+        target: 'http://localhost:5000/',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()],
+});
