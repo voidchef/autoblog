00080e816ddbdaa0e645311075c0800749825e58
voidchef <neelutpal.saha@s.amity.edu>
2023-08-08 15:23:24 +0000

    feat: initialize redux

---
+++ front/src/store.ts
@@ -0,0 +1,12 @@
+import rootReducer from './reducers';
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store;
