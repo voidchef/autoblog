3475afe3c999ba7ccf87a306417f4734116e1ae3
voidchef <neelutpal.saha@s.amity.edu>
2023-10-29 08:46:25 +0000

    chore: cleanup

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,9 +1,7 @@
 import { IUserDoc } from './modules/user/user.interfaces';
-import { IBlogDoc } from './modules/blog/blog.interfaces';
 
 declare module 'express-serve-static-core' {
   export interface Request {
     user: IUserDoc;
-    blog: IBlogDoc;
   }
 }
