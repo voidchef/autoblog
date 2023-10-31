d666fd6d1d7e62ccde55950bd2a994afaf165f0e
voidchef <neelutpal.saha@s.amity.edu>
2023-10-31 17:21:46 +0000

    fix: alert display

--- front/src/components/elements/Common/Alerts.tsx
+++ front/src/components/elements/Common/Alerts.tsx
@@ -6,31 +6,46 @@ import Alert from '@mui/material/Alert';
 import { Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import AlertTitle from '@mui/material/AlertTitle';
+import Slide from '@mui/material/Slide';
 
 const Alerts = () => {
   const alerts = useSelector<RootState, AlertPayload[]>((state) => state.alert);
   const dispatch = useDispatch();
 
   return (
-    <Box width="50%" right={0} position="fixed" zIndex={10000} display={'none'} justifyContent={'flex-end'} padding={'2rem'}>
+    <Box
+      width="50%"
+      right={0}
+      position="fixed"
+      zIndex={10000}
+      display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'flex-end'}
+      padding={'2rem'}
+    >
       <Fragment>
         {alerts &&
           alerts.length > 0 &&
           alerts.map((alert) => (
-            <Alert
-              className="fade error-alert slide-in-top"
-              onClose={() =>
-                dispatch({
-                  type: REMOVE_ALERT,
-                  payload: alert.alertId,
-                })
-              }
-              severity={alert.alertType ?? 'info'}
-              key={alert.alertId}
-            >
-              <AlertTitle>{alert.alertType}</AlertTitle>
-              {alert.msg}
-            </Alert>
+            <div key={alert.alertId}>
+              <Slide direction="up" in={true} timeout={500}>
+                <Alert
+                  className="spaced-alert"
+                  onClose={() =>
+                    dispatch({
+                      type: REMOVE_ALERT,
+                      payload: alert.alertId,
+                    })
+                  }
+                  severity={alert.alertType ?? 'info'}
+                  key={alert.alertId}
+                  sx={{ marginBottom: '10px' }}
+                >
+                  <AlertTitle>{alert.alertType}</AlertTitle>
+                  {alert.msg}
+                </Alert>
+              </Slide>
+            </div>
           ))}
       </Fragment>
     </Box>
