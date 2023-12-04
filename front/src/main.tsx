f6d20bcb684694027610f6ef35578eb8fdd97162
voidchef <neelutpal.saha@s.amity.edu>
2023-12-04 17:18:10 +0000

    chore: update dependencies

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import Provider from 'react-redux/es/components/Provider';
+import { Provider } from 'react-redux'
 import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
