9c508aa7fcf0b4c389c093593576f76dda3d2afc
voidchef <neelutpal.saha@s.amity.edu>
2023-12-16 23:23:36 +0000

    chore: use env variable for target url

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/v1': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    server: {
+      proxy: {
+        '/v1': {
+          target: env.VITE_SERVER_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  plugins: [react(), svgr()],
+    plugins: [react(), svgr()],
+  };
 });
