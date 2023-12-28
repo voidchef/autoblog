8f7ec17b3fde20882d6b306da9ce4a10bb66eb84
voidchef <neelutpal.saha@s.amity.edu>
2023-12-28 19:15:05 +0000

    chore: add retry mechanism for file downloads

--- src/modules/imgGen/download.ts
+++ src/modules/imgGen/download.ts
@@ -3,7 +3,38 @@ import * as fs from 'fs';
 import * as https from 'https';
 import * as path from 'path';
 
+async function downloadFile(url: string, filePath: string, timeout: number): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const fileStream = fs.createWriteStream(filePath);
+    const request = https.get(url, { timeout });
+
+    request.on('response', (response) => {
+      if (response.statusCode === 200 && response.headers['content-type']?.startsWith('image')) {
+        response.pipe(fileStream);
+        fileStream.on('finish', () => {
+          fileStream.close();
+          resolve();
+        });
+      } else {
+        reject(new Error(`Failed to download ${url}. Status code: ${response.statusCode}`));
+      }
+    });
+
+    request.on('error', (err) => {
+      reject(new Error(`Error downloading ${url}: ${err.message}`));
+    });
+
+    request.on('timeout', () => {
+      request.destroy();
+      reject(new Error(`Request to ${url} timed out after ${timeout} ms.`));
+    });
+  });
+}
+
 async function downloadFiles(links: string[], downloadLocation: string): Promise<void> {
+  const maxRetries = 3;
+  const timeoutMs = 5000; // 5 seconds timeout
+
   try {
     // Create the download location directory if it doesn't exist
     if (!fs.existsSync(downloadLocation)) {
@@ -17,29 +48,21 @@ async function downloadFiles(links: string[], downloadLocation: string): Promise
       const fileName = `${counter}.img`;
       const filePath = path.join(downloadLocation, fileName);
 
-      // Download the file
-      // eslint-disable-next-line no-await-in-loop
-      await new Promise<void>((resolve, reject) => {
-        const fileStream = fs.createWriteStream(filePath);
-        https
-          .get(link, (response) => {
-            if (response.statusCode === 200) {
-              response.pipe(fileStream);
-              fileStream.on('finish', () => {
-                fileStream.close();
-                resolve();
-              });
-            } else {
-              reject(new Error(`Failed to download ${link}. Status code: ${response.statusCode}`));
-            }
-          })
-          .on('error', (err) => {
-            reject(new Error(`Error downloading ${link}: ${err.message}`));
-          });
-      });
+      let retries = 0;
 
-      console.log(`Downloaded: ${link}`);
-      counter += 1;
+      // Retry mechanism
+      while (retries < maxRetries) {
+        try {
+          // eslint-disable-next-line no-await-in-loop
+          await downloadFile(link, filePath, timeoutMs);
+          console.log(`Downloaded: ${link}`);
+          counter += 1;
+          break; // Break out of the retry loop if successful
+        } catch (error: any) {
+          console.error(`Error downloading ${link} (Retry ${retries + 1}/${maxRetries}):`, error.message);
+          retries += 1;
+        }
+      }
     }
 
     console.log('All files downloaded successfully.');
